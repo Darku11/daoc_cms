548815637fbcd06383604c6804e11ed4a3868a5d
Aldhran DAoC Solutions <31976137+Darku11@users.noreply.github.com>
2026-08-12 06:34:34 +0200

    Remove public template path disclosure

--- includes/template_engine.php
+++ includes/template_engine.php
@@ -31,15 +31,9 @@ function cms_render_template(string $template_name, array $variables = []) {
     } elseif (file_exists($fallback_file)) {
         $file_to_load = $fallback_file;
     } else {
-        // Fatal error with exact path info, so we never have to guess blindly
-        die("<div style='background:#fff; color:#000; padding:20px; border-left:5px solid red; font-family:sans-serif; z-index:9999; position:relative;'>
-            <strong>Architecture error: template not found!</strong><br><br>
-            The CMS looked for: <code>{$template_name}.php</code><br>
-            Paths searched:<br>
-            1. Theme path: <code>{$theme_file}</code><br>
-            2. Fallback path: <code>{$fallback_file}</code><br><br>
-            Please create the file at one of these paths.
-        </div>");
+        throw new RuntimeException(
+            "Template not found: {$template_name}.php; searched {$theme_file} and {$fallback_file}"
+        );
     }
 
     // 5. Isolate and unpack the template variable scope.
